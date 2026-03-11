b5cf7b3d6613a0b266eaedb2ea44039982c473a9
sonjeesencheva-a11y <sonjeesencheva@gmail.com>
2026-03-11 21:10:51 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,18 +8,18 @@ OWNER_ID = int(os.getenv("OWNER_ID", "0"))
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "Bot e aktivен.\n"
-        "Како owner, прати ми .txt фајл со chat IDs за broadcast."
+        "Bot e aktiven.\n"
+        "Kako owner, prati mi .txt fajl so chat IDs za broadcast."
     )
 
 async def handle_file(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.effective_user.id != OWNER_ID:
-        await update.message.reply_text("Само owner може да праќа фајл со IDs.")
+        await update.message.reply_text("Samo owner moze da praka fajl so IDs.")
         return
 
     doc: Document = update.message.document
     if not doc.file_name.endswith(".txt"):
-        await update.message.reply_text("Качи .txt фајл со по еден chat_id во ред.")
+        await update.message.reply_text("Kaci .txt fajl so po eden chat_id vo red.")
         return
 
     file = await doc.get_file()
@@ -35,24 +35,26 @@ async def handle_file(update: Update, context: ContextTypes.DEFAULT_TYPE):
             except ValueError:
                 pass
 
-    await update.message.reply_text(f"Найдени {len(chat_ids)} chat IDs. Пуштам broadcast...")
+    await update.message.reply_text(f"Najdeni {len(chat_ids)} chat IDs. Pustam broadcast...")
 
-    text = "Поздрав, ова е тест порака од ботот за факултет 🙂"
+    text = "Pozdrav, ova e test poraka od botot 🙂"
 
     for cid in chat_ids:
         try:
             await context.bot.send_message(chat_id=cid, text=text)
             await asyncio.sleep(0.1)
         except Exception as e:
-            print(f"Грешка за {cid}: {e}")
+            print(f"Greska za {cid}: {e}")
 
-    await update.message.reply_text("Готово, пораката е испратена до сите ID-и од фајлот (доколку се валидни).")
+    await update.message.reply_text("Gotovo, porakata e ispratena.")
 
-async def main():
+def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.Document.ALL, handle_file))
-    await app.run_polling()
+
+    app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
